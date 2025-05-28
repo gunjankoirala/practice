476aeb2c26ace1c82486fc3f83106be0cb3e8cf5
Gunjan koirala <gunjankoirala60@gmail.com>
2025-05-28 06:48:33 +0545

    fixed syntax

--- inheritance/super.cs
+++ inheritance/super.cs
@@ -1,24 +1,23 @@
-// using System;
-// namespace inheritance{
-//     public class Person{
-//         public virtual void introduce(){
-//             Console.WriteLine("HI this is gunjan");
-//         }
-//     }
-//     public class Student : Person{
+using System;
+
+    public class Person{
+        public virtual void introduce(){
+            Console.WriteLine("HI this is gunjan");
+        }
+    }
+    public class Student : Person{
         
-//         public  override void introduce()
-//         {
-//             base.introduce();
-//             Console.WriteLine("HI this is student");
-//         }
-//     }
+        public  override void introduce()
+        {
+            base.introduce();
+            Console.WriteLine("HI this is student");
+        }
+    }
      
-//      public class Super{
-//         public static void Main(String[] args)
-//         {
-//             Student s = new Student();
-//             s.introduce();
-//         }
-//      }
-// }
+     public class Super{
+        public static void Main(String[] args)
+        {
+            Student s = new Student();
+            s.introduce();
+        }
+     }
