bbff032cfd13b1a7012bfa1306540440071d9532
Gunjan koirala <gunjankoirala60@gmail.com>
2025-05-28 17:43:47 +0545

    Generics example added

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// public class Box<T>
+// {
+    
+//     public void DisplayItem(T item)
+//     { 
+//         Console.WriteLine("The box has " +item);
+//     }
+
+// }
+// public class DriverClass{
+//      public static void Main(String[] args)
+//     {
+//         Box<int> b= new Box<int>();
+//         Box<String> s= new Box<String>();
+//         s.DisplayItem("knife");
+//         b.DisplayItem(5);
+//     }
+// }
+using System;
+
+public class SimpleList<T>
+{
+    private T[] items;
+    private int count;       
+    private int capacity;    
+
+    public SimpleList(int initialCapacity = 4)
+    {
+        capacity = initialCapacity;
+        items = new T[capacity];
+        count = 0;
+    }
+
+    public void Add(T item)
+    {
+        if (count == capacity)
+        {
+            Resize();
+        }
+        items[count] = item;
+        count++;
+    }
+
+    private void Resize()
+    {
+        capacity *= 2;
+        T[] newItems = new T[capacity];
+        Array.Copy(items, newItems, count);
+        items = newItems;
+    }
+
+    public void Display()
+    {
+        Console.Write("Items: ");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(items[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+public class DriverClass
+{
+    public static void Main(string[] args)
+    {
+        SimpleList<string> list = new SimpleList<string>();
+
+        list.Add("Apple");
+        list.Add("Banana");
+        list.Add("Cherry");
+        list.Add("Date");
+        list.Add("Elderberry");  
+
+        list.Display();
+    }
+}
+
+
